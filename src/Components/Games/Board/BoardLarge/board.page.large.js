b062d129b98ed2fcbcc3ea53f29d178e55cac128
CoderJoe82 <coderjoe82@gmail.com>
2021-08-07 11:47:25 -0400

    adding board page content

--- src/Components/Games/Board/BoardLarge/board.page.large.js
+++ src/Components/Games/Board/BoardLarge/board.page.large.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react'
 import './board.page.large.css'
 import GameMenu from '../../../GameMenu/game.menu'
 import GamesPageHomeButton from '../../../Games/HomeButton/games.page.large.home.button'
+import ScheduleButton from '../../ScheduleButton/schedule.button'
 
 class BoardPageLarge extends Component {
     render() {
         return (
             <div id = "BoardPageLarge">
                 <GameMenu />
+                <h1 id = "boardGamesTitle">Board Games</h1>
+                <ScheduleButton />
+                <div id = "GameBoard">
+                    
+                </div>
                 <GamesPageHomeButton />
             </div>
         )
